c5f4aae2b39c7473bba053c68fc84d5d96c5687b
Waszok <48237746+Waszok@users.noreply.github.com>
2020-11-26 22:58:15 +0100

    Update settings.py

--- langi/langi/settings.py
+++ langi/langi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#s7fwn$q@(p7_93@anu@87a%cjt4qv8j5ai%hgbde0$cc7b(uu'
+SECRET_KEY = '###'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
